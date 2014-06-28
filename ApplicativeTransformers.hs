2084e2a9ddf1a5042d59dc175083001b806dcd35
nadirs <nadirsampaoli@gmail.com>
2014-06-28 17:11:42 +0200

    Implement mapIdentityT

--- ApplicativeTransformers.hs
+++ ApplicativeTransformers.hs
@@ -3,6 +3,7 @@
 
 module ApplicativeTransformers
     ( IdentityT(..)
+    , mapIdentityT
     ) where
 
 import Control.Applicative
@@ -10,3 +11,6 @@ import Control.Applicative
 
 newtype IdentityT (f :: * -> *) a = IdentityT { runIdentityT :: f a }
                                     deriving (Show, Functor, Applicative)
+
+mapIdentityT :: (f a -> g b) -> IdentityT f a -> IdentityT g b
+mapIdentityT h = IdentityT . h . runIdentityT
